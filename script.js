fd940fc4196a5d49e61826f004b4f2e61ff8d3b3
Andrey Oliveira <andreyoliveira222@gmail.com>
2025-03-24 20:24:35 -0300

    Funções

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-var nome = prompt("Qual o seu nome?")
-var sobrenome = prompt("Qual seu sobrenome?")
+var area = document.getElementById('area')
 
-console.log("Olá " + nome + " " + sobrenome)
+function entrar() {
+    var nome = prompt("Digite seu nome")
 
-document.write("<h1>Bem vindo " + nome + "</h1>")
+    if (nome == '' || nome == null) {
+        alert("Ops algo deu errado")
+        area.innerHTML = 'Clique no botão para acessar...'
+    } else {
+        area.innerHTML = 'Bem vindo ' + nome + " "
 
-document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto do Steve'/>")
+        let botaoSair = document.createElement("button")
+        botaoSair.innerText = 'Sair da conta'
+
+        botaoSair.onclick = sair
+
+        area.appendChild(botaoSair)
+    }
+}
+
+function sair() {
+    alert("Até mais")
+    area.innerHTML = "Você saiu"
+}
+
+function mediaAluno(nota1, nota2) {
+    var media = (nota1 + nota2) / 2;
+
+    if (media => 7) {
+        console.log("Aluno aprovado com a média: " + media)
+    } else if (media < 7) {
+        console.log("Aluno reprovado com a média: " + media)
+    }
+}
+
+function aluno(nome, curso) {
+    var mensagem = "Seja bem vindo " + nome + " ao curso de " + curso;
+
+    console.log(mensagem)
+}
