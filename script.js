9712a36908d0f5e5b1d90e9a27034810f6e02516
Andrey Oliveira <andreyoliveira222@gmail.com>
2025-06-11 20:18:09 -0300

    Projeto - Calculadora de IMC

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-// Switch -> Casp
-function pedir() {
-    let valor = prompt("Digite um valor de 1 a 4: ")
+/*
+    Abaixo de 16 Muito abaixo do peso
+    Entre 17 e 18,49 Abaixo do peso
+    Entre 18,5 e 24,99 Peso normal
+    Entre 25 e 29,99 Acima do peso
+*/
+// peso / (altura*altura)
 
-    switch (Number(valor)) {
-        case 1:
-            alert("Você escolheu 1 = suco")
-            break
+var peso;
+var altura;
+var imc;
+var resultado;
 
-        case 2:
-            alert("Você escolheu 2 = Água gelada")
-            break
+function calcular(event) {
+    event.preventDefault(); //Prefenção do comportamento por padrão
 
-        case 3:
-            alert("Você escolheu 3 = Sorvete")
-            break
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
 
-        case 4:
-            alert("Você chamou o garçom")
-            break
+    imc = peso / (altura * altura);
+    resultado = document.getElementById('resultado');
 
-        default:
-            alert("Escolha uma opção entre 1 a 4")
-            break
+    if (imc < 17) {
+        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Cuidado você está muito abaixo do peso!';
+    } else if (imc > 17 && imc <= 18.49) {
+        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está abaixo do peso!';
+    } else if (imc >= 18.5 && imc < 24.99) {
+        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está no peso ideal!';
+    } else if (imc > 25 && imc <= 29.9) {
+        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está acima do peso!';
+    } else if (imc >= 30) {
+        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está com obesidade!';
     }
 
+    document.getElementById('peso').value = '';
+    document.getElementById('altura').value = '';
 }
-
-// Condicionais
-var numero = 10
-
-numero === 10 ? console.log("Numero é igual a 10") : console.log('Numero não tem o valor 10')
