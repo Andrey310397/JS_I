7fce8f384299f84eb22f9020d31cd3d8d04573d8
Andrey Oliveira <andreyoliveira222@gmail.com>
2025-06-13 15:57:48 -0300

    Temporizadores

--- script.js
+++ script.js
@@ -1,37 +1,14 @@
-/*
-    Abaixo de 16 Muito abaixo do peso
-    Entre 17 e 18,49 Abaixo do peso
-    Entre 18,5 e 24,99 Peso normal
-    Entre 25 e 29,99 Acima do peso
-*/
-// peso / (altura*altura)
+// setInterval -> Basicamente executa de tempos em tempos, infinitamente. A cada 1000, é equivalente a 1 segundo.
+// Para pará-lo, basta atribuir a uma variável e atribuir ao comando clearInterval(nomeDaVariável).
 
-var peso;
-var altura;
-var imc;
-var resultado;
-
-function calcular(event) {
-    event.preventDefault(); //Prefenção do comportamento por padrão
-
-    peso = document.getElementById('peso').value;
-    altura = document.getElementById('altura').value;
-
-    imc = peso / (altura * altura);
-    resultado = document.getElementById('resultado');
-
-    if (imc < 17) {
-        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Cuidado você está muito abaixo do peso!';
-    } else if (imc > 17 && imc <= 18.49) {
-        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está abaixo do peso!';
-    } else if (imc >= 18.5 && imc < 24.99) {
-        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está no peso ideal!';
-    } else if (imc > 25 && imc <= 29.9) {
-        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está acima do peso!';
-    } else if (imc >= 30) {
-        resultado.innerHTML = '<br>Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está com obesidade!';
-    }
-
-    document.getElementById('peso').value = '';
-    document.getElementById('altura').value = '';
+function acao() {
+    document.writeln('Executando... <br />');
 }
+// var timer = setInterval(() => {
+//     document.writeln('Executando!!! <br />');
+// }, 1000);
+
+
+// setTimeOut -> Executa apenas uma vez, aquilo que for passado.
+
+setTimeout(acao, 3000);
